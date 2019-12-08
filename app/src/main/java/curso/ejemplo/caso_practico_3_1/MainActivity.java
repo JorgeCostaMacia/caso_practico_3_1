455dace0d50d3037b713c2200ae8d2590fd55618
Jorge <costamaciajorge@gmail.com>
2019-12-08 21:34:55 +0100

    Task2
    Initial

--- app/src/main/java/curso/ejemplo/caso_practico_3_1/MainActivity.java
+++ app/src/main/java/curso/ejemplo/caso_practico_3_1/MainActivity.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager mSensorManager;
+
+    // Individual light and proximity sensors.
+    private Sensor mSensorProximity;
+    private Sensor mSensorLight;
+
+    // TextViews to display current sensor values
+    private TextView mTextSensorLight;
+    private TextView mTextSensorProximity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +30,60 @@ public class MainActivity extends AppCompatActivity {
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
-        StringBuilder sensorText = new StringBuilder();
+        mTextSensorLight = (TextView) findViewById(R.id.label_light);
+        mTextSensorProximity = (TextView) findViewById(R.id.label_proximity);
 
-        for (Sensor currentSensor : sensorList ) {
-            sensorText.append(currentSensor.getName()).append(
-                    System.getProperty("line.separator")
-            );
+        String sensor_error = getResources().getString(R.string.error_no_sensor);
+
+        if (mSensorLight == null) {
+            mTextSensorLight.setText(sensor_error);
         }
 
-        TextView sensorTextView = (TextView) findViewById(R.id.sensor_list);
-        sensorTextView.setText(sensorText);
+        if (mSensorProximity == null) {
+            mTextSensorProximity.setText(sensor_error);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mSensorProximity != null) {
+            mSensorManager.registerListener(this, mSensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorLight != null) {
+            mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int sensorType = sensorEvent.sensor.getType();
+        float currentValue = sensorEvent.values[0];
+
+        switch (sensorType) {
+            case Sensor.TYPE_LIGHT:
+                mTextSensorLight.setText(getResources().getString(R.string.label_light, currentValue));
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                mTextSensorProximity.setText(getResources().getString(R.string.label_proximity, currentValue));
+                break;
+            default:
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
     }
 }
